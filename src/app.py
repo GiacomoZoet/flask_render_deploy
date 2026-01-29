2966c7c6425f322f96046051c966cf6ec0cc3276
giacomozoet <giaccommoo@gmail.com>
2026-01-29 13:22:21 +0100

    initial commit

---
+++ src/app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    username = os.getenv("USERNAME")
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+    print(username, email, password)
+
+    return f'<h1>Hello, World!{username}<br>{email}<br>{password}</h1>', 200
+
+
+def status_404(error):
+    return '<h1>404</h1>', 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
